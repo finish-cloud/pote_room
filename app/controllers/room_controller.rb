e4610cfaad861e22652b3c6a1e19250f318d5f93
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-04-06 13:01:13 +0900

    room controllerを作成

---
+++ app/controllers/room_controller.rb
@@ -0,0 +1,28 @@
+class RoomController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def listiong
+  end
+
+  def pricing
+  end
+
+  def description
+  end
+
+  def photo_upload
+  end
+
+  def location
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'room/index'
+  get 'room/new'
+  get 'room/create'
+  get 'room/listiong'
+  get 'room/pricing'
+  get 'room/description'
+  get 'room/photo_upload'
+  get 'room/location'
+  get 'room/update'
   devise_for :users
   root to:'rooms#index'
 
