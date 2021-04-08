8311b5f1443b3837d704e9ddeb36704016c51db0
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-04-08 10:52:40 +0900

    reservation controllerを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true, length:{maximum: 50}
-
-  has_many :rooms
 end
